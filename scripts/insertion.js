fc50cf45c44019781d5f92d883998494d7f73ecb
pk435 <p.ksingh562001@gmail.com>
2021-11-08 13:54:09 +0530

    Added insertion sort

---
+++ scripts/insertion.js
@@ -0,0 +1,36 @@
+async function insertion(){
+    console.log("Insertion");
+    const arr = document.querySelectorAll('.bar');
+    for(let i=1;i<arr.length;i++){
+        // console.log("in ith loop");
+
+        let key = arr[i].style.height;
+        let j;  
+        for(j=i-1;j>=0;j--){
+            // console.log("in jth loop");
+            arr[j].style.background = "purple";
+            if(parseInt(arr[j].style.height) > parseInt(key)){
+                // console.log("in if cond");
+                await waitforme(delay);
+                arr[j+1].style.height = arr[j].style.height;
+            }else{
+                break;
+            }
+        }
+        await waitforme(delay);
+        arr[j+1].style.height = key;
+    }
+    arr[arr.length-1].style.background = "purple";
+}
+
+const insertionSortbtn = document.querySelector(".insertionSort");
+insertionSortbtn.addEventListener('click', async function(){
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    await insertion();
+    enableSortingBtn();
+    enableSizeSlider();
+    enableNewArrayBtn();
+});
+
