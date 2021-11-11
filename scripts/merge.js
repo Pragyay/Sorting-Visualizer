5362efd9254ca5d86351eaef20c3b614e6695cae
pk435 <p.ksingh562001@gmail.com>
2021-11-11 16:26:44 +0530

    Added merge sort

---
+++ scripts/merge.js
@@ -0,0 +1,97 @@
+async function merge(arr,left,right,mid){
+
+    console.log("in merge()");
+    console.log(`left=${left},mid=${mid},right=${right}`);
+
+    let left_arr_length = mid-left+1;
+    let right_arr_length = right-mid;
+    
+    console.log(`left_arr_length=${left_arr_length}, right_arr_length=${right_arr_length}`);
+
+    let left_arr = new Array(left_arr_length);
+    let right_arr = new Array(right_arr_length);
+
+    for(let i=0;i<left_arr.length;i++){
+        await waitforme(delay);
+        left_arr[i] = arr[left+i].style.height;
+    }
+
+    console.log("added elements to left_arr");
+     
+    for(let i=0;i<right_arr.length;i++){
+        await waitforme(delay);
+        right_arr[i] = arr[mid+1+i].style.height;
+    }
+
+    console.log("added elements to right_arr");
+
+    let i = 0;
+    let j = 0;
+    let k = left;
+
+    while(i<left_arr_length && j<right_arr_length){
+        await waitforme(delay);
+
+        // console.log(typeof(left_arr[i]));  
+
+        // use parseInt to convert string to int (left_arr[i] is of type string)
+        if(parseInt(left_arr[i])<parseInt(right_arr[j])){              
+            arr[k].style.height = left_arr[i];
+            i++;
+        }else{
+            arr[k].style.height = right_arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    console.log("sorted elements");
+    for(let x=left;x<=k;x++){
+        console.log(arr[x].style.height);
+    }
+
+    while(i<left_arr_length){
+        await waitforme(delay);
+        arr[k].style.height = left_arr[i];
+        i++;
+        k++;
+    }
+
+    while(j<right_arr_length){
+        await waitforme(delay);
+        arr[k].style.height = right_arr[j];
+        j++;
+        k++;
+    }
+
+    console.log("sorted remaining elements");
+
+}
+
+async function mergeSort(arr,left,right){
+    console.log("mergeSort()");
+    if(left>=right){    
+        console.log("arr length = 1. returning");
+        return;
+    }
+    const mid = Math.floor((left + right)/2);      // .floor to convert double to int
+    console.log("mid= ",mid);
+    await mergeSort(arr,left,mid);
+    await mergeSort(arr,mid+1,right);
+    await merge(arr,left,right,mid);
+}
+
+const mergeSortbtn = document.querySelector(".mergeSort");
+mergeSortbtn.addEventListener('click', async function(){
+    let arr = document.querySelectorAll(".bar");
+    let left = 0;
+    let right = arr.length-1; 
+    disableSortingBtn();
+    disableSizeSlider();
+    disableNewArrayBtn();
+    await mergeSort(arr,left,right);
+    enableSortingBtn();
+    enableSizeSlider();
+    enableNewArrayBtn();
+    console.log("button click called func");
+});
